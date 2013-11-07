706dc552d10da0b9aec5737d604bb261f2a69426
JoyBar <github@kuriersport.de>
2013-11-07 21:34:43 +0100

    'werwiewas'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+History::Application.config.secret_token = 'bf45f7556096c04ab4d269fb05a98a1d88e2ecf82094a24df66aa750eddd0eaec77d64b3b30ce40c6a7a0b1b3cd6bb1d80d7ad9a8a9232970f8667718a1c8c85'
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Chancellor.create( :first_name => 'Konrad', :last_name => 'Adenauer', :birthday => '05.01.1876', :day_of_death => '19.04.1967', :inauguration => '15.09.1949' )
+Chancellor.create( :first_name => 'Ludwig', :last_name => 'Erhard', :birthday => '05.02.1876', :day_of_death => '19.04.1967', :inauguration => '15.09.1949' )
+Chancellor.create( :first_name => 'Kurt', :last_name => 'Kiesi', :birthday => '05.03.1876', :day_of_death => '19.04.1967', :inauguration => '15.09.1949' )
+Chancellor.create( :first_name => 'Willy', :last_name => 'Brandt', :birthday => '05.04.1876', :day_of_death => '19.04.1967', :inauguration => '15.09.1949' )
+Chancellor.create( :first_name => 'Walter', :last_name => 'Scheel', :birthday => '05.05.1876', :day_of_death => '19.04.1967', :inauguration => '15.09.1949' )
+Chancellor.create( :first_name => 'Helmut', :last_name => 'Schmidt', :birthday => '05.06.1876', :day_of_death => '19.04.1967', :inauguration => '15.09.1949' )
